b4efe45d0823fa125c7e59f612ee2e95eb196a05
JanisataMJ <namuy.2547@gmail.com>
2025-07-26 17:51:45 +0700

    backend begin

---
+++ backend/controller/gender/gender.go
@@ -0,0 +1,33 @@
+package genders
+
+
+import (
+
+   "net/http"
+
+
+   "github.com/JanisataMJ/WebApp/config"
+
+   "github.com/JanisataMJ/WebApp/entity"
+
+   "github.com/gin-gonic/gin"
+
+)
+
+
+func GetAll(c *gin.Context) {
+
+
+   db := config.DB()
+
+
+   var genders []entity.Gender
+
+   db.Find(&genders)
+
+
+   c.JSON(http.StatusOK, &genders)
+
+
+}
+
